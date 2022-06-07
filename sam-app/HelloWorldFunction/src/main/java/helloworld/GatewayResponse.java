69ad5f55910655a473c52bc79164064cc83c36ba
Amol  Johare <amol.johare@acc.ltd>
2022-06-07 16:44:49 +0530

    five commit

--- sam-app/HelloWorldFunction/src/main/java/helloworld/GatewayResponse.java
+++ sam-app/HelloWorldFunction/src/main/java/helloworld/GatewayResponse.java
@@ -1,33 +1,33 @@
-package helloworld;
+// package helloworld;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+// import java.util.Collections;
+// import java.util.HashMap;
+// import java.util.Map;
 
-/**
- * POJO containing response object for API Gateway.
- */
-public class GatewayResponse {
+// /**
+//  * POJO containing response object for API Gateway.
+//  */
+// public class GatewayResponse {
 
-    private final String body;
-    private final Map<String, String> headers;
-    private final int statusCode;
+//     private final String body;
+//     private final Map<String, String> headers;
+//     private final int statusCode;
 
-    public GatewayResponse(final String body, final Map<String, String> headers, final int statusCode) {
-        this.statusCode = statusCode;
-        this.body = body;
-        this.headers = Collections.unmodifiableMap(new HashMap<>(headers));
-    }
+//     public GatewayResponse(final String body, final Map<String, String> headers, final int statusCode) {
+//         this.statusCode = statusCode;
+//         this.body = body;
+//         this.headers = Collections.unmodifiableMap(new HashMap<>(headers));
+//     }
 
-    public String getBody() {
-        return body;
-    }
+//     public String getBody() {
+//         return body;
+//     }
 
-    public Map<String, String> getHeaders() {
-        return headers;
-    }
+//     public Map<String, String> getHeaders() {
+//         return headers;
+//     }
 
-    public int getStatusCode() {
-        return statusCode;
-    }
-}
+//     public int getStatusCode() {
+//         return statusCode;
+//     }
+// }
