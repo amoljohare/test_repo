a1561c52bd2d03295a57b1ef71b804335ad1a5ab
Amol  Johare <amol.johare@acc.ltd>
2022-06-07 17:01:56 +0530

    six commit

--- sam-app/HelloWorldFunction/src/main/java/helloworld/App.java
+++ sam-app/HelloWorldFunction/src/main/java/helloworld/App.java
@@ -16,29 +16,24 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
  */
 public class App implements RequestHandler<Object, Object> {
 
-    public Object handleRequest(final Object input, final Context context) {
-    //     Map<String, String> headers = new HashMap<>();
-    //     headers.put("Content-Type", "application/json");
-    //     headers.put("X-Custom-Header", "application/json");
-    //     try {
-    //         final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-    //         String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
-    //         return new GatewayResponse(output, headers, 200);
-    //     } catch (IOException e) {
-    //         return new GatewayResponse("{}", headers, 500);
-    //     }
-    // }
+    public Object handleRequest(Object input, Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+        try {
+            String pageContents = this.getPageContents("https://checkip.amazonaws.com");
+            String output2 = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
+            String output = output2 + " Amol:  ";
+            return new GatewayResponse(output, headers, 200);
+        } catch (IOException e) {
+            return new GatewayResponse("{}", headers, 500);
+        }
+    }
 
-    // private String getPageContents(String address) throws IOException{
-    //     URL url = new URL(address);
-    //     try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-    //         return br.lines().collect(Collectors.joining(System.lineSeparator()));
-    //     }
-    // }
-    @Override
-	public String handleRequest(Object input, com.amazonaws.services.lambda.runtime.Context context) {
-		System.out.println("hello word amol");
-		return "hello word...amol";
-	
-	}
+    private String getPageContents(String address) throws IOException{
+        URL url = new URL(address);
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            return br.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+    }
 }
